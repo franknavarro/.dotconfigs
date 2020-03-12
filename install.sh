5742d07b85b036d91ef46072394c8a0c79a64d34
Frank Navarro <fnavarro7777@gmail.com>
2020-03-12 21:24:54 +0000

    Added node install

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ LINUX_PACKAGES=$DOTFILES/packages.txt
 sudo apt-get clean && sudo apt-get update;
 xargs sudo apt-get install -y <$LINUX_PACKAGES
 
+bash $DOTFILES/node/install.sh
 bash $DOTFILES/neovim/install.sh
 bash $DOTFILES/tmux/install.sh
---
+++ node/install.sh
@@ -0,0 +1,10 @@
+command -v node || {
+	export PROFILE=$DOTFILES/profile_auto
+	export NVM_DIR=$HOME/.nvm
+  NVM_VERSION=0.34.0
+  RUN mkdir -p ${NVM_DIR} &&
+  curl https://raw.githubusercontent.com/creationix/nvm/v${NVM_VERSION}/install.sh | bash &&
+  . $NVM_DIR/nvm.sh &&
+  nvm install --lts && 
+  nvm use --lts
+}
