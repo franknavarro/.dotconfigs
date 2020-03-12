7f91c3010119552a397726e243c189c8867a1f43
Frank Navarro-Velasco <franknavarro@users.noreply.github.com>
2020-03-12 12:39:03 -0700

    Added yes flag to package installs

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DOTFILES=$(pwd)
 
 LINUX_PACKAGES='./linux/packages.txt'
 sudo apt-get clean && sudo apt-get update;
-xargs sudo apt-get install <$LINUX_PACKAGES
+xargs sudo apt-get install -y <$LINUX_PACKAGES
 
 bash $DOTFILES/neovim/install.sh
 
